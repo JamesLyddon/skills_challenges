d86dacdc8c7075d45efc481b0d9d4701c5b285b1
james lyddon <jameslyddon@gmail.com>
2025-06-10 11:55:01 +0100

    bump

--- lib/debug_challenge_1.py
+++ lib/debug_challenge_1.py
@@ -7,7 +7,6 @@ def get_most_common_letter(text):
     letter = sorted(counter.items(), key=lambda item: item[1], reverse=True)[0][0]
     return letter
 
-
 print(f"""
 Running:  get_most_common_letter("the roof, the roof, the roof is on fire!"))
 Expected: o
