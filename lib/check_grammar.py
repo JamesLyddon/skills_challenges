75e6b8a84281d7c3b5a367979e4019bfea8b5a93
james lyddon <jameslyddon@gmail.com>
2025-06-10 10:58:36 +0100

    refactor

--- lib/check_grammar.py
+++ lib/check_grammar.py
@@ -2,6 +2,4 @@ def check_grammar(str):
     end_chars = {'.', '?', '!'}
     if str is None:
         raise Exception('please include a string to check')
-    if str[0].isupper() and str[-1] in end_chars:
-        return True
-    return False
+    return str[0].isupper() and str[-1] in end_chars
